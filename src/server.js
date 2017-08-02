62da82eb2411d1aa2f522b6dee4d48c8aa419cc6
Rodney <workinntn@outlook.com>
2017-08-01 21:15:52 -0400

    Added dataset for users

--- src/server.js
+++ src/server.js
@@ -3,45 +3,60 @@
 var http = require('http');
 
 var books = [
-        {
-            id: 1,
-            series: "Hank the Cowdog",
-            seriesNo: 1,
-            title: "The Case of the Wandering Goat",
-            yearPublished: 2010,
-            isbn: new Date().toISOString(),
-        },
-        {
-            id: 2,
-            series: "Hank the Cowdog",
-            seriesNo: 2,
-            title: "The Sheet Monster",
-            yearPublished: 2015,
-            isbn: new Date().toISOString(),
-        },
-        {
-            id: 3,
-            series: "Hank the Cowdog",
-            seriesNo: 3,
-            title: "Eddie the Rac",
-            yearPublished: 2017,
-            isbn: new Date().toISOString(),
-        },
-        {
-            id: 4,
-            title: "Once Upon A Time In America",
-            yearPublished: 1950,
-            isbn: new Date().toISOString(),
-        },
-        {
-            id: 5,
-            series: "The Chronicles of Narnia",
-            seriesNo: 1,
-            title: "The Lion, The Witch and the Wardrobe",
-            yearPublished: 1950,
-            isbn: new Date().toISOString(),
-        },
-    ]
+    {
+        id: 1,
+        series: "Hank the Cowdog",
+        seriesNo: 1,
+        title: "The Case of the Wandering Goat",
+        yearPublished: 2010,
+        isbn: new Date().toISOString(),
+    },
+    {
+        id: 2,
+        series: "Hank the Cowdog",
+        seriesNo: 2,
+        title: "The Sheet Monster",
+        yearPublished: 2015,
+        isbn: new Date().toISOString(),
+    },
+    {
+        id: 3,
+        series: "Hank the Cowdog",
+        seriesNo: 3,
+        title: "Eddie the Rac",
+        yearPublished: 2017,
+        isbn: new Date().toISOString(),
+    },
+    {
+        id: 4,
+        title: "Once Upon A Time In America",
+        yearPublished: 1950,
+        isbn: new Date().toISOString(),
+    },
+    {
+        id: 5,
+        series: "The Chronicles of Narnia",
+        seriesNo: 1,
+        title: "The Lion, The Witch and the Wardrobe",
+        yearPublished: 1950,
+        isbn: new Date().toISOString(),
+    },
+]
+
+var users = [
+    {
+        id: 1,
+        userId: 'workinntn@hotmail.com',
+        firstName: 'Rodney',
+        lastName: 'Dixon',
+    },    
+    {
+        id: 2,
+        userId: 'hankanddrover@outlook.com',
+        firstName: 'Isaiah',
+        lastName: 'Dixon',
+    },    
+]
 
 http.createServer( (req, res) => {
     res.setHeader('Content-Type', 'application/json');
